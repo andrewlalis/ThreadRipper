e4ea40079c5768fd984c421f5462dc301879d315
Andrew Lalis <andrewlalisofficial@gmail.com>
2020-09-11 22:38:18 +0200

    Added basic files.

---
+++ src/main/java/nl/andrewlalis/threadripper/ThreadRipperApplication.java
@@ -0,0 +1,10 @@
+package nl.andrewlalis.threadripper;
+
+/**
+ * Main application starting point.
+ */
+public class ThreadRipperApplication {
+	public static void main(String[] args) {
+		System.out.println("Hello world.");
+	}
+}
